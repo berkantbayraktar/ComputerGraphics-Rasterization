6600db0308b5a87333227757469b85db0a0ff818
ilker <ilkeraycicek36@gmail.com>
2018-12-02 23:46:30 +0300

    Add function templates

--- source/rasterizer.cpp
+++ source/rasterizer.cpp
@@ -42,6 +42,10 @@ void translate_triangle(Triangle* triangle,Translation translation);
 void rotate_triangle(Triangle * triangle,Rotation rotation);
 void scale_triangle(Triangle* triangle,Scaling scaling);
 bool cull_triangle(Triangle* triangle);
+void cam_transform(Triangle *triangle , double M_cam[4][4]);
+void per_transform(Triangle *triangle , double M_per[4][4]);
+void per_divide(Triangle *triangle);
+void vp_transform(Triangle *triangle , double M_vp[3][4]);
 
 /*
 	Initializes image with background color
@@ -64,6 +68,37 @@ void initializeImage(Camera cam) {
 	Using types in "hw2_types.h" and functions in "hw2_math_ops.cpp" will speed you up while working.
 */
 void forwardRenderingPipeline(Camera cam) {
+
+
+    double u_x,u_y,u_z,v_x,v_y,v_z,w_x,w_y,w_z,e_x,e_y,e_z,n,f,t,l,b,r,n_x,n_y;
+    
+    // Camera related points and vectors
+    u_x = cam.u.x; u_y = cam.u.y ; u_z = cam.u.z; // u
+    v_x = cam.v.x; v_y = cam.v.y ; v_z = cam.v.z; // v
+    w_x = cam.w.x; w_y = cam.w.y ; w_z = cam.w.z; // w
+    e_x = cam.pos.x; e_y = cam.pos.y ; e_z = cam.pos.z; // e
+    n = cam.n; f = cam.f; // near, far
+    t = cam.t; b = cam.b; r = cam.r;l = cam.l; // top,right,left,bottom
+    n_x = cam.sizeX; n_y = cam.sizeY; // height and width of the camera
+
+    // Camera transformation matrix
+    double M_cam[4][4] = { { u_x , u_y , u_z , -(u_x*e_x + u_y*e_y + u_z*e_z) },
+                    { v_x , v_y , v_z , -(v_x*e_x + v_y*e_y + v_z*e_z) },
+                    { w_x , w_y , w_z , -(w_x*e_x + w_y*e_y + w_z*e_z) },
+                    { 0 , 0 , 0 , 1 }};
+
+    // Perspective transformation matrix
+    double M_per[4][4] = { { 2*n/(r-l) , 0 , (r+l)/(r-l) , 0 },
+                    { 0 , 2*n/(t-b) , (t+b)/(t-b) , 0 },
+                    { 0 , 0 , -(f+n)/(f-n) , -(2*f*n) /(f-n)},
+                    { 0 , 0 , -1 ,0 }};
+
+    // Viewport transformation matrix
+    double M_vp[3][4] = { { n_x/2 , 0 , 0 , (n_x-1)/2 },
+                    { 0 , n_y/2 , 0 , (n_y-1)/2 },
+                    {0 ,0 , 0.5 , 0.5} };
+
+              
     // Traverse all models
     for(int i = 0; i < numberOfModels;i++)
     {   
@@ -116,10 +151,16 @@ void forwardRenderingPipeline(Camera cam) {
                 }
 
                 // b) CAMERA TRANSFORMATION
+                    cam_transform(triangle,M_cam);
 
-                // c) PROJECTION TRANSFORMATION
+                // c) PERSPECTIVE TRANSFORMATION
+                    per_transform(triangle,M_per);
+                
+                // d) PERSPECTIVE DIVIDE 
+                    per_divide(triangle);
 
                 // 2) CULLING (Pipeline 3.step)
+                
                 // Do culling if backfaced culling is enabled
                 if(backfaceCullingSetting == 1)
                 {
@@ -132,7 +173,7 @@ void forwardRenderingPipeline(Camera cam) {
                 }
 
                 // 3) VIEWPORT TRANSFORMATION (Pipeline 4.step)
-
+                    vp_transform(triangle,M_vp);
 
                 // 4) TRIANGLE RASTERIZATION (Pipeline 5.step)
 
@@ -143,6 +184,26 @@ void forwardRenderingPipeline(Camera cam) {
             }
         }
     }
+}
+
+
+void vp_transform(Triangle *triangle , double M_vp[3][4])
+{
+
+}
+
+void cam_transform(Triangle *triangle , double M_cam[4][4])
+{
+
+}
+
+void per_transform(Triangle *triangle , double M_per[4][4])
+{
+
+}
+
+void per_divide(Triangle *triangle)
+{
 
 }
 
@@ -156,12 +217,10 @@ void translate_triangle(Triangle* triangle,Translation translation)
 
 }
 
-
 void rotate_triangle(Triangle * triangle,Rotation rotation)
 {
 
 }
-
 
 
 void scale_triangle(Triangle* triangle,Scaling scaling)
